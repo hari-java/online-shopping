c7fd135c17965bca14071d6608c03e0f8871cec4
hari <harikrishna.kandala1@gmail.com>
2018-06-08 12:13:43 +0400

    My project setup completed successfully

---
+++ onlineshoppingbackend/src/main/java/com/hari/onlineshoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.hari.onlineshoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
